f5d9cf500a5fb9445052db5ff177accc9242bdf8
zwz <akazwz@pm.me>
2021-10-23 22:53:24 +0800

    Initial commit

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export const Index = () => {
+  return (
+    <View className='index'>
+      <Text>Hello world!</Text>
+    </View>
+  )
+}
