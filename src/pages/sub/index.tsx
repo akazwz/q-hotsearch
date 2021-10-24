f060afab8c3b269a162f8ab4426bc0268d56ee2a
zwz <akazwz@pm.me>
2021-10-24 17:08:24 +0800

    订阅词管理

--- src/pages/sub/index.tsx
+++ src/pages/sub/index.tsx
@@ -1,25 +1,156 @@
-import { View, Text, Button } from '@tarojs/components'
+import { useState } from 'react'
 import Taro from '@tarojs/taro'
+import { useReady } from '@tarojs/runtime'
+import { View } from '@tarojs/components'
+import Button from '@taroify/core/button'
+import '@taroify/core/button/style'
+import '@taroify/core/tag/style'
+import Cell from '@taroify/core/cell'
+import '@taroify/core/cell/style'
+import SwipeCell from '@taroify/core/swipe-cell'
+import '@taroify/core/swipe-cell/style'
+import Field from '@taroify/core/field'
+import '@taroify/core/field/style'
+import Divider from '@taroify/core/divider'
+import '@taroify/core/divider/style'
+import { AddOutlined } from '@taroify/icons'
 import './index.scss'
 
 const Sub = () => {
+  const [subs, setSubs] = useState([])
+  const [keyword, setKeyword] = useState('')
+
+  // 获取订阅
+  useReady(() => {
+    getSubWords()
+  })
+
+  const getSubWords = () => {
+    Taro.request({
+      url: 'https://api.hellozwz.com/v1/subs',
+      header: {
+        token: Taro.getStorageSync('token')
+      },
+      method: 'GET'
+    }).then((res) => {
+      const { data } = res.data
+      setSubs(data)
+    }).catch((err) => {
+      console.log('error:' + err)
+    })
+  }
+
+  const addSubWord = (word: string) => {
+    Taro.request({
+      url: 'https://api.hellozwz.com/v1/subs',
+      header: {
+        token: Taro.getStorageSync('token')
+      },
+      data: {
+        sub_word: word
+      },
+      method: 'POST'
+    }).then((res) => {
+      if (res.statusCode === 201) {
+        getSubWords()
+      }
+    }).catch((err) => {
+      console.log('error:' + err)
+    })
+  }
+
+  const deleteSubWord = (word: string) => {
+    Taro.request({
+      url: 'https://api.hellozwz.com/v1/subs',
+      header: {
+        token: Taro.getStorageSync('token')
+      },
+      data: {
+        sub_word: word
+      },
+      method: 'DELETE'
+    }).then((res) => {
+      if (res.statusCode === 204) {
+        getSubWords()
+      }
+    }).catch((err) => {
+      console.log('error:' + err)
+    })
+  }
+
   return (
-    <View className='index'>
-      <Text>通知</Text>
-      <Button onClick={() => {
-        Taro.requestSubscribeMessage({
-          tmplIds: ['XV16ZyG6Af_gG8D4qg7M17Fw23m_zYWNo689XpJKYQE'],
-          fail: () => {
-            console.log('fail')
-          },
-          success: () => {
-            console.log('success')
-          }
-        }).then()
-      }}
+    <View>
+      <Cell.Group inset>
+        <Field
+          align='center'
+          label='订阅词'
+          placeholder='请输入订阅词'
+          value={keyword}
+          onChange={(value) => {
+            setKeyword(value.detail.value)
+          }}
+        >
+          <Field.Button>
+            <Button
+              size='medium'
+              color='primary'
+              shape='round'
+              icon={<AddOutlined />}
+              onClick={() => {
+                addSubWord(keyword)
+              }}
+            >
+              添加
+            </Button>
+          </Field.Button>
+        </Field>
+      </Cell.Group>
+      <Button
+        block
+        color='info'
+        onClick={() => {
+          Taro.requestSubscribeMessage({
+            tmplIds: ['XV16ZyG6Af_gG8D4qg7M17Fw23m_zYWNo689XpJKYQE'],
+            fail: () => {
+              console.log('fail')
+            },
+            success: () => {
+              console.log('success')
+            }
+          }).then()
+        }}
       >
         获取通知
       </Button>
+      <Divider
+        style={{
+          color: '#1989fa',
+          borderColor: '#1989fa',
+          padding: '0 16px'
+        }}
+      >
+        订阅管理
+      </Divider>
+      {subs.map((sub, index) => {
+        return <SwipeCell key={index}>
+          <Cell bordered={false} title={sub}>
+            滑动进行编辑
+          </Cell>
+          <SwipeCell.Actions side='right'>
+            <Button
+              variant='contained'
+              shape='square'
+              color='danger'
+              onClick={() => {
+                deleteSubWord(sub)
+              }}
+            >
+              删除
+            </Button>
+            <Button variant='contained' shape='square' color='primary'>编辑</Button>
+          </SwipeCell.Actions>
+        </SwipeCell>
+      })}
     </View>
   )
 }
