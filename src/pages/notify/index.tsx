c5986c9d8f6503817c41b6ff81b1d1ee20e10066
zwz <akazwz@pm.me>
2021-10-30 18:24:51 +0800

    get notify

--- src/pages/notify/index.tsx
+++ src/pages/notify/index.tsx
@@ -3,8 +3,25 @@ import { useDidShow, useTabItemTap } from '@tarojs/runtime'
 import Taro from '@tarojs/taro'
 import Button from '@taroify/core/button'
 import '@taroify/core/button/style'
+import { useState } from 'react'
+
+export interface Notify {
+  NotifyCount: number,
+  NotifyGap: number,
+  AllNotifyCount: number,
+  NotifyLimitCount: number,
+  LastNotify: string
+}
 
 const NotifyManage = () => {
+  const [notify, setNotify] = useState({
+    NotifyCount: 0,
+    NotifyGap: 0,
+    AllNotifyCount: 0,
+    NotifyLimitCount: 0,
+    LastNotify: '',
+  })
+
   // tap vibrate
   useTabItemTap(() => {
     Taro.vibrateShort().then()
@@ -19,6 +36,24 @@ const NotifyManage = () => {
       },
     }).then((res) => {
       console.log(res)
+      if (res.statusCode !== 200) {
+        console.log('err')
+        return
+      }
+      const { code, data } = res.data
+      if (code !== 2000) {
+        console.log('error')
+        return
+      }
+
+      const { notify_count, notify_gap, all_notify_count, notify_limit_count, last_notify } = data
+      setNotify({
+        NotifyCount: notify_count,
+        NotifyGap: notify_gap,
+        AllNotifyCount: all_notify_count,
+        NotifyLimitCount: notify_limit_count,
+        LastNotify: last_notify,
+      })
     })
   })
 
@@ -46,6 +81,11 @@ const NotifyManage = () => {
       >
         获取通知
       </Button>
+      {notify.NotifyCount}
+      {notify.NotifyGap}
+      {notify.AllNotifyCount}
+      {notify.NotifyLimitCount}
+      {notify.LastNotify}
     </View>
   )
 }
